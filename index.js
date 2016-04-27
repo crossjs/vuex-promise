b288518dc5ca5acb5b3636673f89696d61973c2c
crossjs <liwenfu@crossjs.com>
2016-04-27 16:14:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+export default function vuexPromise ({
+  debug = false
+  status = {
+    PENDING: 0,
+    SUCCESS: 1,
+    FAILURE: 2
+  }
+} = {}) {
+  return {
+    onInit () {
+      if (debug) {
+        console.log('Vuex Promise Initialized.')
+      }
+    },
+    onMutation ({ type, payload: [payload] }, state, store) {
+      if (payload && payload.then) {
+        store.dispatch(type, null, status.PENDING)
+        payload.then(
+          res => store.dispatch(type, res, status.SUCCESS),
+          err => store.dispatch(type, err, status.FAILURE)
+        )
+      }
+    }
+  }
+}
