0c6a4bcd93bb7dbe3f6b15f226356a2d3b0dcbad
crossjs <liwenfu@crossjs.com>
2016-07-07 22:11:22 +0800

    [build] 1.0.1

---
+++ .tools/release.sh
@@ -0,0 +1,27 @@
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # test
+  npm run test
+
+  # build
+  rm -rf dist
+  mkdir dist
+  npm run build
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+fi
